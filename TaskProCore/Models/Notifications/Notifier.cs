ec04e86ed9535321a27ad0a67fa744615fab89cc
CaioCeesaar <caio2001lc@gmail.com>
2024-07-23 01:54:41 -0300

    initial addDbContext + notifier configuration

--- TaskProCore/Models/Notifications/Notifier.cs
+++ TaskProCore/Models/Notifications/Notifier.cs
@@ -1,6 +1,24 @@
-﻿namespace TaskProCore.Models.Notifications;
+﻿using System.Collections.Generic;
+using TaskProCore.Interfaces;
 
-public class Notifier
+namespace TaskProCore.Models.Notifications;
+
+public class Notifier : INotifier
 {
+    private readonly List<Notification>  _notifications = new();
     
+    public bool HaveNotification()
+    {
+        return _notifications.Count != 0;
+    }
+
+    public List<Notification> GetNotifications()
+    {
+        return _notifications;
+    }
+
+    public void Handle(Notification notification)
+    {
+        _notifications.Add(notification);
+    }
 }
--- TaskProInfra/Data/AppDbContext.cs
+++ TaskProInfra/Data/AppDbContext.cs
@@ -1,4 +1,14 @@
-﻿public class AppDbContext
+﻿using Microsoft.EntityFrameworkCore;
+using TaskProCore.Models;
+
+public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
     
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<Task> Tasks { get; set; }
+    public DbSet<ProjectTask> ProjectTasks { get; set; }
 }
